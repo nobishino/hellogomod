f6648afaa06d39e28cc78ae6beb60d79ac6e5251
nobishino <nobishii11@gmail.com>
2021-01-23 15:35:07 +0900

    Remove named import

--- hello.go
+++ hello.go
@@ -1,15 +1,13 @@
 package hellogomod
 
-import (
-	quoteV3 "rsc.io/quote/v3"
-)
+import "rsc.io/quote/v3"
 
 // Hello returns Hello, world.
 func Hello() string {
-	return quoteV3.HelloV3()
+	return quote.HelloV3()
 }
 
 // Proverb returns proverb
 func Proverb() string {
-	return quoteV3.Concurrency()
+	return quote.Concurrency()
 }
