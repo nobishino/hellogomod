fc1b26632924f0b5c0afd555622e7ca926f7879c
nobishino <nobishii11@gmail.com>
2021-01-23 15:22:04 +0900

    Add Proverb function

--- hello.go
+++ hello.go
@@ -1,8 +1,16 @@
 package hellogomod
 
-import "rsc.io/quote"
+import (
+	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
+)
 
 // Hello returns Hello, world.
 func Hello() string {
 	return quote.Hello()
+}
+
+// Proverb returns proverb
+func Proverb() string {
+	return quoteV3.Concurrency()
 }
